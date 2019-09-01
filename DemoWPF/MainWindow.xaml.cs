8c1f94ef632e0d453713e57e8089c513a6ad2436
Adem Velika <adiadm@hotmail.com>
2019-09-01 20:49:25 +0200

    add WPF GUI

---
+++ DemoWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DemoApplication.Interfaces.UseCases;
+using DemoApplication.Respositories;
+using DemoApplication.UseCases;
+using DemoInfrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IPersonRespository res = new PersonRespository();
+
+            IGetPersonInteractor usercase = new GetPersonUseCase(res);
+            var result = usercase.Hanlde("");
+
+            grida.ItemsSource = result;
+        }
+    }
+}
